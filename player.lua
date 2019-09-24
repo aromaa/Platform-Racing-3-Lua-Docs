6e0366191b8da1007c12e68828356c271a52617f
infinilator-v2204 <54496756+infinilator-v2204@users.noreply.github.com>
2019-09-24 19:16:57 +0300

    Player FOV
    Also added "player." in usage examples.

--- player.lua
+++ player.lua
@@ -25,16 +25,20 @@ accel = 50
 --- Strength of the gravity affecting the player.
 gravity = 1
 
+--- Affects how far can you see (zoom). Greater values decrease your view size.
+fov = 3
+
 --- Hurts the player by a specific amount and with a specified velocity.
 --- @tparam int damage The amount of damage done to the player. By default 1.
 --- @tparam number vel The strength of the impact that pushes the player. By default 0.3.
+--- @usage player.hurt(2, 0.15)
 function hurt(damage, vel)
 end
 
 --- Sets a metadata with a specified key-value pair to the player.
 --- @tparam string key Name of the metadata variable (the key)
 --- @param value Value of the metadata variable
---- @usage setmetadata("coins", 10)
+--- @usage player.setmetadata("coins", 10)
 function setmetadata(key, value)
 end
 
@@ -42,20 +46,21 @@ end
 --- @tparam string key The key (metadata variable name) that's used to find its value.
 --- @param defaultValue The value to return if the searched key does not exists. By default no specific value is returned.
 --- @return Returns the key's value. Returns defaultValue instead if the key isn't found.
---- @usage getmetadata("coins", 0)
+--- @usage player.getmetadata("coins", 0)
 function getmetadata(key, defaultValue)
 end
 
 --- Sends an alert popup to the player.
 --- @tparam string message The message to show the player.
+--- @usage player.alert("Hello world!")
 function alert(message)
 end
 
 --- Sends a chat message to the player.
 --- @tparam string message The message to show to the player.
 --- @tparam int color The decimal (or hex) value of the RGB color to use. By default #1D5497.
---- @usage chat('hello', 0x1D5497)
---- @usage chat('hello', 1922199)
+--- @usage player.chat('hello', 0x1D5497)
+--- @usage player.chat('hello', 1922199)
 function chat(message, color)
 end
 
@@ -66,21 +71,21 @@ end
 --- Gives the player a laser gun item with specified settings.
 --- Fields: ammo(int), reload(int), damage(int), recoil(float), knockback(float), sap(int), recovery(float), speed(float), range(float), rotation(String), repeat(float), phasing(float)
 --- @param settings The given laser gun item as an AS3 Object.
---- @usage givelaser(toobject{ammo=5,reload=4})
+--- @usage player.givelaser(toobject{ammo=5,reload=4})
 function givelaser(settings)
 end
 
 --- Gives the player a sword item with specified settings.
 --- Fields: ammo(int), reload(int), damage(int), recoil(float), knockback(float), sap(int), recovery(float)
 --- @param settings The given sword item as an AS3 Object.
---- @usage givesword(toobject{ammo=5,reload=4})
+--- @usage player.givesword(toobject{ammo=5,reload=4})
 function givesword(settings)
 end
 
 --- Gives the player a rocket launcher item with specified settings.
 --- Fields: ammo(int), reload(int), damage(int), recoil(float), knockback(float), sap(int), recovery(float), speed(float), accel(float), maxVel(float), range(float), rotation(String), repeat(float), phasing(float)
 --- @param settings The given rocket launcher item as an AS3 Object.
---- @usage giverocket(toobject{ammo=5,reload=4})
+--- @usage player.giverocket(toobject{ammo=5,reload=4})
 function giverocket(settings)
 end
 
@@ -88,11 +93,13 @@ end
 --- @tparam number xpos Teleport the player (xpos) blocks among the x-axis.
 --- @tparam number ypos Teleport the player (ypos) blocks among the y-axis. A positive y teleports the player down.
 --- @tparam boolean keepvelocity Determines if the player keeps their velocity after teleportation. False by default.
+--- @usage player.teleportto(3, -1, true)
 function teleportto(xpos, ypos, keepvelocity)
 end
 
 --- Rotates a player to a given rotation at a given speed.
 --- @param value (float) Sets the angle that the player will be rotated to in the nearest increment of 90.
 --- @param amplifier (Number) Sets the speed amplifier of the rotation speed. For reference, Fez rotates at 10 amplifier.
+--- @usage player.rotate(90, 1.6)
 function rotate(value, amplifier)
 end
