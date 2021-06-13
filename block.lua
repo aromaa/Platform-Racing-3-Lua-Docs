8972ce9469d29aaaa68c0ddb1a71923e273ed782
CoralCOasa <59053310+CoralCOasa@users.noreply.github.com>
2021-06-13 14:19:10 +0300

    Update block.lua

--- block.lua
+++ block.lua
@@ -10,6 +10,12 @@ paused = false
 --- Can the block be moved
 canmove = false
 
+--- The x-coordinate of the block's position.
+xpos = 0
+
+--- The y-coordinate of the block's position.
+ypos = 0
+
 --- Tries to move the block to a specified direction. Move delay and the option to move the player can be passed, too.
 --- @tparam string direction The direction to move the block to.
 --- @tparam number moveDelay The delay of how long it takes to the block to be able to be moved again. By default 100 (ms).
@@ -30,9 +36,20 @@ end
 function teleportto(xpos, ypos, removecurrentblock, removetargetspot)
 end
 
+--- Teleports the block to the given coordinates. NOTE: This is only client-side, meaning this function will only work on the person's client who touched the block.
+--- @tparam int xpos The x-coordinate to teleport the block to.
+--- @tparam int ypos The y-coordinate to teleport the block to.
+--- @tparam boolean removecurrentblock If the original block should be deleted among a successful teleportation. True by default.
+--- @tparam boolean removetargetspot If true, the teleporting block teleports to its specified location and deletes the block if its spot is taken up. False by default.
+--- @treturn boolean Returns whether the move was successful.
+--- @usage block.teleporttopos(0, -7, false, true)
+function teleporttopos(xpos, ypos, removecurrentblock, removetargetspot)
+end
+
 --- Makes the block vanish and reappear later. The vanish delay can be passed.
 --- @tparam int time The amount of time until vanishing. Vanishes instantly by default.
-function vanish(time)
+--- @tparam int vanishduration The amount of time it takes for the block to reappear after being vanished. 2000 by default.
+function vanish(time, vanishduration)
 end
 
 --- Makes the block explode, damaging the player in the process.
@@ -53,6 +70,25 @@ end
 
 --- Destroys the block by shattering it.
 function shatter()
+end
+
+--- Applies paint to the block, altering its appearance until the paint wears off.
+--- @tparam int color The decimal (or hex) value of the RGB color of the paint. By default 16777215.
+--- @tparam int duration How long does the paint last? By default 999999.
+--- @tparam number alpha Strength of the paint, ranges from 0 to 1.
+--- @usage block.dye(255, 50, 0.5)
+function dye(color, duration, alpha)
+end
+
+--- Summons a (client-sided) black hole.
+--- @tparam number duration The duration of the black hole. By default 9900.
+--- @tparam number strength The strength of the player pulling/pushing effect of the black hole. By default 1.
+--- @tparam int offsetx How many blocks away to the right does the black hole spawn? By default 0.
+--- @tparam int offsety How many blocks away downwards does the black hole spawn? By default 0.
+--- @tparam int speedx How fast does the black hole move to right? By default 0.
+--- @tparam int speedy How fast does the black hole move downwards? By default 0.
+--- @usage block.blackhole(9900, 1, 0, 0, 0, 0)
+function blackhole(duration, strength, offsetx, offsety, speedx, speedy)
 end
 
 --- Finds another block by given coordinates. A player reference can be passed.
